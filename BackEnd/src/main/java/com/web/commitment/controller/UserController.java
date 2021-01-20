7b14d2c978caeb9ccb3d5293c0ac2480128c34d8
DESKTOP-KVCQHCD\multicampus <1693013@naver.com>
2021-01-20 11:16:16 +0900

    [#S04P12A308-32] feature/signup : Add signup and modify

--- BackEnd/src/main/java/com/web/commitment/controller/UserController.java
+++ BackEnd/src/main/java/com/web/commitment/controller/UserController.java
@@ -1,5 +1,46 @@
 package com.web.commitment.controller;
 
-public class UserController {
+import javax.transaction.Transactional;
+import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.web.commitment.dao.UserDao;
+import com.web.commitment.dto.User;
+
+import io.swagger.annotations.ApiOperation;
+
+@CrossOrigin
+@RestController
+public class UserController {
+	@Autowired
+	UserDao userDao;
+
+	@PostMapping("/account/signup")
+	@ApiOperation(value = "회원가입수정")
+	@Transactional
+	public User signup(@Valid @RequestBody User request) {
+		User user = userDao.findUserByEmail(request.getEmail());// 수정
+
+		if (user == null)// 가입
+			user = request;
+		else {
+			user.setEmail(request.getEmail());
+			user.setPass(request.getPass());
+			user.setNickname(request.getNickname());
+			user.setTel(request.getTel());
+			user.setAge(request.getAge());
+			user.setGender(request.getGender());
+			user.setMystory(request.getMystory());
+		}
+
+		System.out.println(user);
+		userDao.save(user);
+
+		return user;
+	}
 }
--- BackEnd/src/main/java/com/web/commitment/dao/UserDao.java
+++ BackEnd/src/main/java/com/web/commitment/dao/UserDao.java
@@ -1,5 +1,17 @@
 package com.web.commitment.dao;
 
-public class UserDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.web.commitment.dto.User;
+
+
+@Repository
+public interface UserDao extends JpaRepository<User, String> {
+
+	User getUserByEmail(String email);
+
+	User findUserByEmail(String email);
+	
+	
 }
