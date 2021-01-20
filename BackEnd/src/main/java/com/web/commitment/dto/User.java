e8655f0cc02c47c2e86c27cf270071c7686eab9d
DESKTOP-KVCQHCD\multicampus <1693013@naver.com>
2021-01-20 10:03:56 +0900

    [S04P12A308-57] feat/signup: add userdto

--- BackEnd/src/main/java/com/web/commitment/dto/User.java
+++ BackEnd/src/main/java/com/web/commitment/dto/User.java
@@ -1,5 +1,80 @@
 package com.web.commitment.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class User {
+
+	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)--> 자동생성 할떄만 사용
+	@Column(name = "email")
+	private String email;
+
+	@JsonIgnore // json 보낼떄 무시(굳이 보낼 필요 없음)
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "nickname", nullable = false, unique = true)
+	private String nickname;
+
+	@Column(name = "tel")
+	private String tel;
+
+	@Column(name = "age")
+	private String age;
+
+	@Column(name = "gender")
+	private String gender;
+
+	@Column(name = "mystory")
+	private String mystory;
+
+	@Column(name = "lat")
+	private String lat;
+
+	@Column(name = "lng")
+	private String lng;
+
+	@Column(name = "badge")
+	private String badge;
+
+	@JsonIgnore // json 보낼떄 무시(굳이 보낼 필요 없음)
+	@Column(name = "authkey")
+	private String authkey;
+
+	@Column(name = "auth")
+	private String auth;
+
+	@Builder
+	public User(String email, String password, String nickname, String tel, String age, String gender, String mystory) {
+		super();
+		this.email = email;
+		this.password = password;
+		this.nickname = nickname;
+		this.tel = tel;
+		this.age = age;
+		this.gender = gender;
+		this.mystory = mystory;
+	}
+	
+	
 
 }
