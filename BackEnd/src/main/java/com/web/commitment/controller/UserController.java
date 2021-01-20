8249879acb4fb28a53dba8be134223fc0997d22c
DESKTOP-KVCQHCD\multicampus <1693013@naver.com>
2021-01-20 11:50:23 +0900

    [#S04P12A308-74] feature/delete: Add delete

--- BackEnd/src/main/java/com/web/commitment/controller/UserController.java
+++ BackEnd/src/main/java/com/web/commitment/controller/UserController.java
@@ -4,12 +4,17 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.web.commitment.dao.UserDao;
+import com.web.commitment.dto.BasicResponse;
 import com.web.commitment.dto.User;
 
 import io.swagger.annotations.ApiOperation;
@@ -42,5 +47,19 @@ public class UserController {
 		userDao.save(user);
 
 		return user;
+	}
+
+	@DeleteMapping("/account/delete")
+	@ApiOperation(value = "회원탈퇴")
+	@Transactional
+	public Object userDelete(@RequestParam(required = true) final String email) {
+		User user = userDao.getUserByEmail(email);
+		userDao.delete(user);
+
+		final BasicResponse result = new BasicResponse();
+		result.status = true;
+		result.data = "success";
+
+		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 }
