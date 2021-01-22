db3e0fe685f77e3f709964aa1edf8b5efb16e1e5
김청화 <asder36@naver.com>
2021-01-22 20:05:03 +0900

    [#S04P12A308-125] feature/commit: Add search user by email or nickname

--- BackEnd/src/main/java/com/web/commitment/dao/UserDao.java
+++ BackEnd/src/main/java/com/web/commitment/dao/UserDao.java
@@ -1,5 +1,6 @@
 package com.web.commitment.dao;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,5 +14,9 @@ public interface UserDao extends JpaRepository<User, String> {
 	Optional<User> findUserByEmail(String email);
 
 	int countByEmail(String email);
+
+	Collection<User> findByNicknameContainingIgnoreCase(String keyword);
+
+	Collection<User> findByEmailContainingIgnoreCase(String keyword);
 	
 }
