3c0fce724654491a8d2b3187159f27f75f34f397
박유정 <1693013@naver.com>
2021-01-25 17:39:02 +0900

    랭킹 controller 와 dao 따로 뺌

--- BackEnd/src/main/java/com/web/commitment/controller/RankingController.java
+++ BackEnd/src/main/java/com/web/commitment/controller/RankingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.web.commitment.dao.CommitDao;
+import com.web.commitment.dao.RankingDao;
 import com.web.commitment.dto.Ranking;
 
 import io.swagger.annotations.ApiOperation;
@@ -16,30 +16,30 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 public class RankingController {
 	@Autowired
-	CommitDao commitDao;
+	RankingDao rankingDao;
 	
 	@GetMapping("/rank/total")
 	@ApiOperation(value = "랭킹")
 	public List<Ranking> commitRank() {
-		List<Ranking> list = commitDao.commitRank();
+		List<Ranking> list = rankingDao.commitRank();
 		return list;
 	}
 	@GetMapping("/rank/aweek")
 	@ApiOperation(value = "주간랭킹")
 	public List<Ranking> commitWeekRank() {
-		List<Ranking> list = commitDao.commitWeekRank();
+		List<Ranking> list = rankingDao.commitWeekRank();
 		return list;
 	}
 	@GetMapping("/rank/month")
 	@ApiOperation(value = "월간랭킹")
 	public List<Ranking> commitMonthRank() {
-		List<Ranking> list = commitDao.commitMonthRank();
+		List<Ranking> list = rankingDao.commitMonthRank();
 		return list;
 	}
 	@GetMapping("/rank/following")
 	@ApiOperation(value = "팔로잉한 사람들 커밋 랭킹")
 	public List<Ranking> followingRank(@RequestParam(required = true) String email) {
-		List<Ranking> list = commitDao.followingRank(email);
+		List<Ranking> list = rankingDao.followingRank(email);
 		return list;
 	}
 }
--- BackEnd/src/main/java/com/web/commitment/dao/CommitDao.java
+++ BackEnd/src/main/java/com/web/commitment/dao/CommitDao.java
@@ -4,12 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.web.commitment.dto.Commit;
-import com.web.commitment.dto.Ranking;
 
 @Repository
 public interface CommitDao extends JpaRepository<Commit, String> {
@@ -19,20 +16,5 @@ public interface CommitDao extends JpaRepository<Commit, String> {
 	Optional<Commit> findByLatAndLng(String lat, String lng);
 
 	List<Commit> findByEmail(String email);
-
-	@Query(value = "select commit.user_email email, rank() over (order by count(*) desc) ranking, count(*) cnt from commit  group by commit.user_email", nativeQuery = true)
-	List<Ranking> commitRank();
-	@Query(value = "select commit.user_email email, rank() over (order by count(*) desc) ranking, count(*) cnt from commit where created_at between DATE_ADD(NOW(),INTERVAL -1 WEEK ) AND NOW() group by commit.user_email", nativeQuery = true)
-	List<Ranking> commitWeekRank();
-	@Query(value = "select commit.user_email email, rank() over (order by count(*) desc) ranking, count(*) cnt from commit where created_at between DATE_ADD(NOW(),INTERVAL -1 MONTH ) AND NOW() group by commit.user_email", nativeQuery = true)
-	List<Ranking> commitMonthRank();
-	@Query(value = "select commit.user_email email, rank() over (order by count(*) desc) ranking, count(*) cnt \r\n" + 
-			"from commit \r\n" + 
-			"where commit.user_email in (\r\n" + 
-			"select follow.follow_to from follow where follow_from=:email) group by commit.user_email;", nativeQuery = true)
-	List<Ranking> followingRank(@Param("email") String email);
-
-//	@Query(value = "select * from commit c where user_email=:email and c.lat=:lat and c.lng=:lng", nativeQuery = true)
-//	Optional<Commit> commitCheck(@Param("email") String email, @Param("lat") String lat, @Param("lng") String lng);
 
 }
