ae6ea8d330038443ce129a098c83d42ded325385
강해성 <kanga94@naver.com>
2021-01-26 13:48:06 +0900

    [#S04P12A308-128] feature/main : Add Menu-buttons

--- FrontEnd/src/views/Main.vue
+++ FrontEnd/src/views/Main.vue
@@ -5,34 +5,29 @@
       color="white"
       flat
     >
-      <v-avatar
+    <div>
+      <!-- <v-avatar
         :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
         size="32"
-      ></v-avatar>
+      ></v-avatar> -->
+      <vs-input placeholder="Search">
+      </vs-input>
+    </div>
 
       <!-- tab 바뀌는부분 -->
-    <vs-tabs alignment="center">
-      <vs-tab label="Home">
-        <div>
-
-        </div>
-      </vs-tab>
-      <vs-tab label="Documents">
-        <div>
-
-        </div>
-      </vs-tab>
-      <vs-tab label="Contributors">
-        <div>
-
-        </div>
-      </vs-tab>
-      <vs-tab label="Ecosystem">
-        <div>
-        </div>
-      </vs-tab>
-    </vs-tabs>
-
+    <div>
+      <v-tabs>
+        <v-tab
+          v-for="item in items"
+          :key="item"
+        >
+        <v-icon>
+          {{ item }}
+        </v-icon>
+        </v-tab>
+      </v-tabs>
+    </div>
+    <!-- 아바타 바뀌는 지역 -->
       <v-avatar
         class="hidden-sm-and-down"
         color="grey darken-1 shrink"
@@ -49,9 +44,9 @@
           >
             <v-sheet
               rounded="lg"
-              min-height="268"
+              min-height="300"
             >
-              <!--  -->
+              <!-- 첫번째 넣어야할거 -->
             </v-sheet>
           </v-col>
 
@@ -63,7 +58,7 @@
               min-height="70vh"
               rounded="lg"
             >
-              <!--  -->
+              <router-view></router-view>
             </v-sheet>
           </v-col>
 
@@ -87,12 +82,22 @@
 <script>
   export default {
     data: () => ({
-      links: [
-        'Dashboard',
-        'Messages',
-        'Profile',
-        'Updates',
-      ],
+      items: [
+          'mdi-home', 'mdi-web', 'mdi-airplane', 'mdi-apple-keyboard-control', 'mdi-account',
+        ],  
     }),
   }
 </script>
+
+<style >
+.v-toolbar__content {
+  display: flex;
+  justify-content: space-between;
+}
+
+v-tab {
+  width: 20px;
+
+}
+
+</style>
