fdf1502f2f6e93145b742fed33e2602b682df49a
강해성 <kanga94@naver.com>
2021-01-27 13:53:16 +0900

    [#S04P12A308-173] feature/main : Add MainPage UX/UI

--- FrontEnd/src/views/Main.vue
+++ FrontEnd/src/views/Main.vue
@@ -10,15 +10,22 @@
         :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
         size="32"
       ></v-avatar> -->
-      <vs-avatar text="Logo"/>
-      <vs-avatar color="primary" text="primary"/>
+      <vs-button circle icon flat >
+         <i class="bx bx-tag"></i>
+      </vs-button>
+      <vs-button circle icon flat >
+         <i class="bx bx-search"></i>
+      </vs-button>
       <vs-input placeholder="Search">
       </vs-input>
     </div>
 
       <!-- tab 바뀌는부분 -->
     <div>
-      <v-tabs>
+      <v-tabs
+      background-color="transparent"
+      
+      >
         <v-tab
           v-for="item in items"
           :key="item"
@@ -34,33 +41,52 @@
       <!-- 일단 정렬을 위한 div -->
     </div>
     <div class="navbar_right">
-      <vs-avatar color="danger" icon="photo_camera" />
-      <vs-avatar color="warning" icon="filter_vintage" />
+      <vs-button
+        circle
+        icon
+        flat
+      >
+        <i class='bx bx-plus' ></i>
+      </vs-button>
+
+      <vs-button
+        
+        circle
+        icon
+        flat
+      >
+        <i class='bx bxl-whatsapp' ></i>
+      </vs-button>
+
+      <vs-button
+       
+        circle
+        icon
+        flat
+      >
+        <i class='bx bx-up-arrow-alt' ></i>
+      </vs-button>
     </div>
     </v-app-bar>
-
+    <!-- 메인페이지파트 -->
     <v-main class="grey lighten-3">
       <v-container>
         <v-row>
           <v-col
+            class="sidebar_left"
             cols="12"
             sm="2"
           >
-            <v-sheet
-              rounded="lg"
-              min-height="300"
-              class="mb-3"
-            >
-              <!-- 첫번째 넣어야할거 -->
-            </v-sheet>
-            <v-sheet
-              rounded="lg"
-              min-height="300">
 
-            </v-sheet>
+            <ProfileSummary />
+              <!-- 첫번째 넣어야할거 -->
+
+            <NotYetAddArticle />
+
           </v-col>
 
           <v-col
+            class="mainslot"
             cols="12"
             sm="8"
           >
@@ -68,7 +94,12 @@
               min-height="70vh"
               rounded="lg"
             >
-              <router-view></router-view>
+              <div class="mainpage">
+                <!-- 일단 3개컴포넌트 넣어보고 길이 결정해보기 -->
+                <FollowerMap />
+                <MyState />
+                <AllArticle />
+              </div>
             </v-sheet>
           </v-col>
 
@@ -78,16 +109,17 @@
           >
           <div class="radar mt-4 mb-3">
             <v-slider
-             v-model="slider"
             :thumb-size="15"
             thumb-label="always"
             ></v-slider>
           </div>
             <v-sheet
               rounded="lg"
-              min-height="268"
+              min-height="200"
             >
+            <CommitNow />
             </v-sheet>
+              
           </v-col>
         </v-row>
       </v-container>
@@ -96,17 +128,36 @@
 </template>
 
 <script>
+  import AllArticle from './components/mainpage/AllArticle'
+  import FollowerMap from './components/mainpage/FollowerMap'
+  import MyState from './components/mainpage/MyState'
+  import CommitNow from './components/mainpage/CommitNow'
+  import NotYetAddArticle from './components/mainpage/NotYetAddArticle'
+  import ProfileSummary from './components/mainpage/ProfileSummary'
+
   export default {
+    name: "MainPage",
+
+    components: {
+      AllArticle,
+      FollowerMap,
+      MyState,
+      CommitNow,
+      NotYetAddArticle,
+      ProfileSummary,
+
+    },
+
     data: () => ({
       items: [
           'mdi-home', 'mdi-web', 'mdi-apple-keyboard-control', 'mdi-account',
-        ], 
-      value1: 1, 
+        ],  
     }),
   }
 </script>
 
 <style >
+
 .navbar_right {
   display: flex;
   width: min-content
@@ -120,8 +171,26 @@
   justify-content: space-between;
 }
 
-v-tab {
-  width: 20px;
+/* .sidebar_left {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 4px;
+}
+
+.mainslot {
+  overflow: auto;
+
+} */
+
+@media (max-width: 450px) {
+
+.search {
+  display: none;
+}
+
+.navbar_right {
+  display: none;
+}
 
 }
 
---
+++ FrontEnd/src/views/components/mainpage/FollowerMap.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-carousel 
+  hide-delimiters
+  show-arrows-on-hover
+  v-model="model" height="40vh">
+    <v-carousel-item
+      v-for="(color, i) in colors"
+      :key="color"
+    >
+      <v-sheet
+        :color="color"
+        height="100%"
+        tile
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="display-3">
+            Slide {{ i + 1 }}
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+    data: () => ({
+      model: 0,
+      colors: [
+        'grey',
+        'orange',
+        'yellow darken-2',
+        'green',
+        'blue ligthen-2',
+      ],
+    }),
+}
+</script>
+
+<style>
+
+</style>
