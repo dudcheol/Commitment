9a9972309edf12533be91d8ec63c1064d1db7206
박유정 <1693013@naver.com>
2021-02-01 14:46:33 +0900

    modify config

--- FrontEnd/vue.config.js
+++ FrontEnd/vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   devServer:{
-    disableHostCheck:true
+    disableHostCheck:true,
+    proxy: {
+      '/': {
+          target: 'http://인스턴스주소:3000',
+            changeOrigin: true
+        }
+    }
   },
   "transpileDependencies": [
     "vuetify"
