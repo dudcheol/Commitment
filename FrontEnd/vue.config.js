6065b887a0696f8c748ace8827a01851997472a4
박유정 <1693013@naver.com>
2021-02-01 15:32:57 +0900

    modify config

--- FrontEnd/vue.config.js
+++ FrontEnd/vue.config.js
@@ -1,15 +1,7 @@
 module.exports = {
   devServer:{
-    disableHostCheck:true,
-    proxy: {
-      '/': {
-          target: 'http://인스턴스주소:3000',
-            changeOrigin: true
-        }
-    }
+    disableHostCheck:true
   },
-  publicPath: "./",
-  outputDir: "../FrontEnd/public",
   "transpileDependencies": [
     "vuetify"
   ]
