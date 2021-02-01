4ca9e501ab574014ff0e088193ad1a99df646e15
강해성 <kanga94@naver.com>
2021-02-01 16:49:44 +0900

    [#S04P12A308-110] feature/header : fixed four tabs

--- FrontEnd/src/views/Index.vue
+++ FrontEnd/src/views/Index.vue
@@ -1,12 +1,20 @@
 <template>
   <v-app id="inspire">
-    <!-- <v-app-bar app color="white" flat hide-on-scroll>
-      <v-avatar
+    <v-app-bar app color="white" flat hide-on-scroll>
+      <!-- <v-avatar
         :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
         size="32"
-      ></v-avatar>
+      ></v-avatar> -->
+      <v-btn elevation="" fab small>
+        <v-icon >
+          mdi-pencil
+        </v-icon>
+      </v-btn>
+      <!-- <vs-button circle icon flat>
+        <i class="bx bx-search"></i>
+      </vs-button>   -->
 
-      <v-tabs centered class="ml-n9" color="grey darken-1">
+      <v-tabs grow centered color="grey darken-1" class="d-flex justify-center">
         <v-tab v-for="item in items" :key="item" :to="item.route">
           <v-icon>
             {{ item.icon }}
@@ -14,9 +22,58 @@
         </v-tab>
       </v-tabs>
 
-      <v-avatar class="hidden-sm-and-down" color="grey darken-1 shrink" size="32"></v-avatar>
-    </v-app-bar> -->
-    <v-app-bar app color="white" flat>
+    <v-speed-dial 
+      direction="bottom"
+      transition="slide-x-reverse-transition"
+    >
+      <template v-slot:activator>
+        <v-btn
+          small
+          color="blue"
+          dark
+          fab
+        >
+          <v-icon v-if="fab">
+            mdi-close
+          </v-icon>
+          <v-icon v-else>
+            mdi-account-circle
+          </v-icon>
+        </v-btn>
+      </template>
+      <v-btn
+        fab
+        dark
+        small
+        color="green"
+      >
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="indigo"
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="red"
+      >
+        <v-icon>mdi-delete</v-icon>
+      </v-btn>
+    </v-speed-dial> 
+
+
+
+
+
+      <!-- <v-avatar class="hidden-sm-and-down" color="grey darken-1 shrink" size="32"></v-avatar> -->
+    </v-app-bar>
+    <!-- <v-app-bar app color="white" flat>
       <div class="d-flex flex-row">
         <vs-button circle icon flat>
           <i class="bx bx-tag"></i>
@@ -50,7 +107,7 @@
           <i class="bx bx-up-arrow-alt"></i>
         </vs-button>
       </div>
-    </v-app-bar>
+    </v-app-bar> -->
 
     <v-main class="grey lighten-3">
       <router-view></router-view>
@@ -74,6 +131,20 @@ export default {
 </script>
 
 <style scoped>
+
+v-speed-dial {
+  z-index: 0 !important
+}
+
+
+@media (max-width: 450px) {
+  .v-tab {
+    min-width: 10vw !important
+
+  }
+}
+  
+
 /* .sidebar_left {
   position: -webkit-sticky;
   position: sticky;
@@ -83,15 +154,15 @@ export default {
 .mainslot {
   overflow: auto;
 
-} */
+}
 
 @media (max-width: 450px) {
-  .search {
-    display: none;
-  }
 
-  .navbar_right {
-    display: none;
+  .tabs__container {
+    max-width: 90vw;
   }
-}
+} */
+
+
+
 </style>
