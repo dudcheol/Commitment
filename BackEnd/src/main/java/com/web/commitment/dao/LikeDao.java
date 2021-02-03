184f04dfc1b5e0d1b514b4f4af53f1737e016ea4
김청화 <asder36@naver.com>
2021-02-03 10:10:28 +0900

    [#S04P13A308-138] feature/paging_sub: Add paging like list

--- BackEnd/src/main/java/com/web/commitment/dao/LikeDao.java
+++ BackEnd/src/main/java/com/web/commitment/dao/LikeDao.java
@@ -3,6 +3,8 @@ package com.web.commitment.dao;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -15,7 +17,7 @@ public interface LikeDao extends JpaRepository<Like, String> {
 
 	Optional<Like> findByEmailAndSnsId(String email, int snsId);
 
-	List<Like> findAllByEmail(String email);
+	Page<Like> findAllByEmail(String email, Pageable pabeable);
 
 	int countByWriter(String email);
 
