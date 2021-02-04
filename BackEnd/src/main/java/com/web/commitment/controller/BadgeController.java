94f0697a954226e233a41f50867b6591c2712c5b
박유정 <1693013@naver.com>
2021-02-04 10:37:52 +0900

    [#S04P12A308-46] fix/뱃지 초기화 수정 : :add 뱃지 초기화 수정

--- BackEnd/src/main/java/com/web/commitment/controller/BadgeController.java
+++ BackEnd/src/main/java/com/web/commitment/controller/BadgeController.java
@@ -30,8 +30,8 @@ public class BadgeController {
 				
 		return count;
 	}
-	@GetMapping("/badge/reset")
-	@ApiOperation(value = "뱃지 초기화(회원 가입시)")
+//	@GetMapping("/badge/reset")
+//	@ApiOperation(value = "뱃지 초기화(회원 가입시)")
 	public String badgeReset(@RequestParam String email) {
 		Badge b=new Badge();
 		b.setUserEmail(email);
@@ -48,7 +48,7 @@ public class BadgeController {
 	//뱃지달성을 어떻게 받아올건지는 프론트와 협의후 적절히 수정예정
 	//객체로받아올 것인가 아니면 뱃지별로 메소드를 제작할것인가...?
 	@PostMapping("/badge/list")
-	@ApiOperation(value = "뱃지")
+	@ApiOperation(value = "뱃지 저장")
 	public String badgeList(@RequestBody Badge b) {
 		badgedao.save(b);
 		return "success";
--- BackEnd/src/main/java/com/web/commitment/controller/UserController.java
+++ BackEnd/src/main/java/com/web/commitment/controller/UserController.java
@@ -45,7 +45,9 @@ public class UserController {
 	@Autowired
 	UserDao userDao;
 	@Autowired
-	ProfileController profilecontroller;
+	ProfileController profileController;
+	@Autowired
+	BadgeController badgeController;
 	@Autowired
 	private JwtService jwtService;
 
@@ -83,6 +85,7 @@ public class UserController {
 
 		return response;
 	}
+
 	@GetMapping("/account/info")
 	@ApiOperation(value = "로그인 회원정보 받아오기")
 	public ResponseEntity<Map<String, Object>> getInfo(HttpServletRequest req) {
@@ -106,7 +109,8 @@ public class UserController {
 	@Transactional
 	public User signup(@Valid @RequestBody User request) {
 		User user = userDao.findUserByEmail(request.getEmail());// 수정
-
+		badgeController.badgeReset(request.getEmail());//뱃지 리셋
+		
 		if (user == null)// 가입
 			user = request;
 		else {
@@ -124,14 +128,7 @@ public class UserController {
 
 		return user;
 	}
-//
-//	@GetMapping("/account/email")
-//	@ApiOperation(value = "이메일로")
-//	public Object user(@RequestParam(required = true) final String email) {
-//		User user = userDao.getUserByEmail(email);
-//		System.out.println(user);
-//		return user;
-//	}
+
 
 	@DeleteMapping("/account/delete")
 	@ApiOperation(value = "회원탈퇴")
@@ -227,11 +224,8 @@ public class UserController {
 					"<img src=\"https://commitmentbucket.s3.ap-northeast-2.amazonaws.com/%EB%A9%94%EC%9D%BC+%EC%9D%B8%EC%A6%9D.PNG\" width=\"550\" >")
 					.append("<br>").append("<a  href='http://localhost:8080/user/signUpConfirm?email=")
 					.append(user.getEmail()).append("&authKey=").append(AuthenticationKey)
-					.append("' target='_blenk'><font size=\"5px\"  color=\"black\">[메일 인증]</a></font>")
-					.append("</div>").append("</center>").toString(), true);// true를 해야 html형식으로 됨
-			
-			
-
+					.append("' target='_blenk'><font size=\"5px\"  color=\"black\">[메일 인증]</a></font>").append("</div>")
+					.append("</center>").toString(), true);// true를 해야 html형식으로 됨
 
 			Transport.send(msg);
 			System.out.println("이메일 전송");
