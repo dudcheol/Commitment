3bbaec5ebd6e35047fed393d92bc2f82c9687233
dudcheol <95_pyc@naver.com>
2021-02-04 17:40:23 +0900

    [ conflict correction ]

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,16 +1,5 @@
 <template>
   <v-app>
-<<<<<<< HEAD
-    <div id="develop-navigation">
-      <router-link to="/">main</router-link> /
-      <router-link to="/signup">signup</router-link> /
-      <router-link to="/login">login</router-link> /
-      <router-link to="/mypage">mypage</router-link> /
-      <router-link to="/rank">ranking</router-link> /
-      <router-link to="/badge">Badge</router-link>
-    </div>
-=======
->>>>>>> 1f9d1dbf1e0dd4cc019a3343bcac7a372d92951f
     <router-view></router-view>
   </v-app>
 </template>
