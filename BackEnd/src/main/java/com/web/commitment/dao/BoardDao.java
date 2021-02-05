a0838438b531b4586df20c9738a1623045592688
박유정 <1693013@naver.com>
2021-02-05 21:32:05 +0900

    [#S04P13A308-162] fix/게시글 내용 추가 : :add 게시글 내용 추가

--- BackEnd/src/main/java/com/web/commitment/dao/BoardDao.java
+++ BackEnd/src/main/java/com/web/commitment/dao/BoardDao.java
@@ -20,10 +20,10 @@ public interface BoardDao extends JpaRepository<Board, String> {
 	List<Board> findBoardByEmail(String to);
 	
 	//내용으로검색
-	@Query(value = "select distinct * from  tag t, sns s, commit c, user u where  s.id=t.sns_id and c.id=s.commit_id and c.user_email=:email and UPPER(s.content) like UPPER(:keyword) and c.open = 1", nativeQuery = true)
+	@Query(value = "select * from sns s,commit c where UPPER(s.content) like UPPER(:keyword) and c.open = 1", nativeQuery = true)
 	Page<Board> findByContentContainingIgnoreCase(@Param("keyword") String keyword, Pageable pageable);
 	//제목으로 검색
-	@Query(value = "select distinct * from  tag t, sns s, commit c, user u where s.id=t.sns_id and c.id=s.commit_id and c.user_email=:email and UPPER(s.title) like UPPER(:keyword) and c.open = 1", nativeQuery = true)
+	@Query(value = "select * from sns s, commit c where  c.id=s.commit_id and UPPER(s.title) like UPPER(:keyword) and c.open = 1", nativeQuery = true)
 	Page<Board> findByTitleContainingIgnoreCase(@Param("keyword") String keyword, Pageable pageable);
 
 	// 랭킹관련
@@ -54,13 +54,14 @@ public interface BoardDao extends JpaRepository<Board, String> {
 	Page<Board> findBoardByEmailSort(@Param("email")String email,Pageable pageable);
 
 	//글쓴이로 검색
-	@Query(value = "select * from sns s,commit c where c.id=s.commit_id and c.open=1 "
+	@Query(value = "select * from sns s, commit c "
+			+ "where c.id=s.commit_id and c.open=1 "
 			+ "and lower(s.user_email) like :keyword order by s.created_at desc", nativeQuery = true)
 	Page<Board> findByEmailContainingIgnoreCase(@Param("keyword") String keyword, Pageable pageable);
 	
 	//제목과 내용으로 검색
-	@Query(value = "select * from tag t, sns s, commit c, user u where c.open=1 "
-			+ "and s.id=t.sns_id and c.id=s.commit_id and c.user_email=:email "
+	@Query(value = "select * from sns s, commit c where c.open=1 "
+			+ "and c.id=s.commit_id  "
 			+ "and (lower(s.title) like :keyword or lower(s.content) like :keyword ) order by s.created_at desc", nativeQuery = true)
 	Page<Board> findByTitleandContent(@Param("keyword")String keyword, Pageable pageable);
 
