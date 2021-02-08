9fd716589c7b13baed258eefec84179c1d67dacf
강해성 <kanga94@naver.com>
2021-02-08 23:41:16 +0900

    [#S04P13A308-185] feature/signup : Add signup dialog

---
+++ FrontEnd/src/components/common/dialog/EmailDialog.vue
@@ -0,0 +1,42 @@
+<template>
+  <vs-dialog width="300px" v-model="dialog" not-center>
+        <template #header>
+          <h4 class="not-margin">
+            {{ content.title }}
+          </h4>
+        </template>
+
+
+        <div class="con-content">
+          {{ content.text }}
+        </div>
+
+        <template #footer>
+          <div class="con-footer">
+            <vs-button @click="yes" transparent>
+              오케바리
+            </vs-button>
+          </div>
+        </template>
+    </vs-dialog>  
+</template>
+
+<script>
+
+export default {
+  name: 'EmailDialog',
+  components: {},
+  props: ['content', 'dialog'],
+  computed: {},
+  created() {},
+  watch: {},
+  methods: {
+    yes() {
+      this.$emit('yes');
+    },
+  },
+  data: function () {
+    return {};
+  },
+};
+</script>
