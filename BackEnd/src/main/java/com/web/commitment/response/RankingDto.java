aad5f512f0ad9908d9a434e97c07689816132d2d
박유정 <1693013@naver.com>
2021-02-15 22:41:55 +0900

    [#S04P13A308-300] feature/modify : add  뱃지 획득 가능여부 수정
    Former-commit-id: 324518e4210a24efea99fed802a0277f5227a32d

---
+++ BackEnd/src/main/java/com/web/commitment/response/RankingDto.java
@@ -0,0 +1,12 @@
+package com.web.commitment.response;
+
+import com.web.commitment.dto.Profile;
+import com.web.commitment.dto.Ranking;
+
+import lombok.Data;
+@Data
+public class RankingDto {
+	private Ranking ranking;
+	private Profile profile;
+
+}
