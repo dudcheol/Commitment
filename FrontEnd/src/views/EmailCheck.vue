30026a610f4966d211ffd2b94911cd7c2ae1cc56
dudcheol <95_pyc@naver.com>
2021-02-17 19:42:04 +0900

    [#S04P13A308-351] feature/final
    Former-commit-id: a4eb77c9c821c6a7dcaec95fbe05558ac6334905

--- FrontEnd/src/views/EmailCheck.vue
+++ FrontEnd/src/views/EmailCheck.vue
@@ -1,18 +1,28 @@
 <template>
-  <div>
+  <div class="blue-grey lighten-5 fill-height">
     <center id="out">
-      <v-card class="mx-auto" max-width="380" id="in">
+      <v-card class="mx-auto pa-4" max-width="380" id="in" rounded="xl" elevation="0">
         <img
           src="https://commitmentbucket.s3.ap-northeast-2.amazonaws.com/%ED%9A%8C%EC%9B%90%EA%B0%80%EC%9E%85.png"
           alt="logo"
         />
         <v-card-text>
-          <p>환영합니다!</p>
-          <p>이메일 인증이 완료되었습니다.</p>
+          <p class="font-weight-black text-h2">환영합니다!</p>
+          <p>이메일 인증이 완료되었습니다</p>
           <p>커밋먼트와 함께 여행을 떠나보세요</p>
         </v-card-text>
         <center>
-          <v-btn color="pink" dark dense flat class="ma-2" @click="maingo()">
+          <v-btn
+            color="primary"
+            outlined
+            dark
+            dense
+            class="ma-2"
+            elevation="0"
+            rounded
+            :ripple="false"
+            @click="maingo()"
+          >
             로그인 페이지로 이동
           </v-btn>
         </center>
@@ -23,17 +33,17 @@
 
 <script>
 export default {
-  name: "EmailCheck",
+  name: 'EmailCheck',
   data() {
     return {};
   },
   created() {},
   methods: {
     mainlink() {
-      this.$router.push({ name: "Login" });
+      this.$router.push({ name: 'Login' });
     },
     maingo() {
-      this.$router.push({ name: "Main" });
+      this.$router.push({ name: 'Main' });
     },
   },
 };
@@ -41,7 +51,7 @@ export default {
 
 <style scoped>
 #but {
-  padding: "100px";
+  padding: '100px';
 }
 img {
   width: 60%;
