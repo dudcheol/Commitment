be29af8db4a6a5a66ec743b39b27c3f628b8875d
김청화 <asder36@naver.com>
2021-02-17 12:53:16 +0900

    [#S04P13A308-314] feature/realteimCommit: realtimeCommit paging
    Former-commit-id: afa3caef00f95a22178a1a22c03886a643a3cae6

--- FrontEnd/src/components/index/main/CommitNow.vue
+++ FrontEnd/src/components/index/main/CommitNow.vue
@@ -20,11 +20,11 @@
         </v-expansion-panel-header>
         <v-expansion-panel-content>
           <commit-card
-            v-for="(item, index) in nowCommits"
+            v-for="(item, index) in openGroups"
             :key="index"
-            :username="item.username"
-            :address="item.address"
-            :img="item.img"
+            :username="item.from"
+            :address="item.dataId"
+            :img="item.profile"
             class="mb-2"
           ></commit-card>
           <v-btn
@@ -34,7 +34,7 @@
             height="52px"
             color="blue-grey darken-4"
             text
-            @click="readMore"
+            @click="getRealtimeCommitData"
             ><strong>더보기</strong></v-btn
           >
         </v-expansion-panel-content>
@@ -54,6 +54,21 @@ export default {
   data() {
     return {
       nowCommits: [],
+      // paging 처리 위함
+      openGroups: [],
+      tempKey: [],
+      tempValue: [],
+      tempArr: [],
+      groupLength: 0,
+      lastKey: '',
+      lastValue: null,
+      isFinish: false,
+      ///
+      perPage: 10,
+      page: {
+        value: '1',
+      },
+      isScroll: false,
     };
   },
   computed: {
@@ -74,20 +89,81 @@ export default {
     firebase
       .database()
       .ref('noti/all')
-      .limitToLast(10)
-      .on('value', (snap) => {
-        let res = snap.val();
-        this.nowCommits = [];
-        for (const idx in res) {
-          res[idx].id = idx;
-          this.nowCommits.unshift({
-            username: res[idx].from,
-            address: res[idx].dataId,
-            img: res[idx].profile,
-            email: res[idx].userEmail,
+      .once('value', (snapshot) => {
+        console.log(snapshot);
+        this.groupLength += snapshot.numChildren();
+
+        console.log('groupLength: ' + this.groupLength); // 알림 총 길이
+        // 최초 리스트 길이가 perPage 보다 작을 경우 최초 리스트 길이만 부르기
+        if (this.groupLength < this.perPage + 1)
+          this.perPage = this.groupLength - 1;
+
+        firebase
+          .database()
+          .ref('noti/all')
+          .limitToLast(this.perPage + 1)
+          .on('child_added', (snapshot) => {
+            this.getData(snapshot);
           });
-        }
       });
+  },
+  methods: {
+    getData(snapshot) {
+      if (!snapshot.val()) return;
+      this.tempArr.push(snapshot.val());
+
+      // this.tempKey = [];
+      this.tempKey.push(snapshot.key); // key
+      console.log('tempKey: ' + this.tempKey);
+
+      this.tempValue.push(snapshot.val()); // 얘는 [object Object]
+      // console.log('tempValue: ' + JSON.stringify(this.tempValue));
+      this.lastKey = this.tempKey[0];
+      // this.lastValue = this.tempValue[0];
+
+      console.log(
+        'groupLength: ' + this.groupLength + ' perPage: ' + this.perPage
+      );
+      if (this.groupLength < this.perPage + 1) {
+        console.log('isFinish: true');
+        this.isFinish = true;
+        this.openGroups.push(snapshot.val()); // 원래 있던 거
+      }
+
+      if (this.tempValue.length === this.perPage + 1) {
+        this.tempArr.shift();
+
+        this.openGroups.push(...this.tempValue.reverse());
+        this.groupLength -= this.perPage;
+      }
+    },
+    getRealtimeCommitData() {
+      if (this.isFinish) return;
+      this.tempArr = [];
+      this.tempKey = [];
+      this.tempValue = [];
+
+      // console.log('lastKey: ' + this.lastKey);
+      // console.log('lastValue: ' + JSON.stringify(this.lastValue));
+      firebase
+        .database()
+        .ref('noti/all')
+        .endAt(this.lastValue, this.lastKey)
+        // .endAt(this.lastKey)
+        .limitToLast(this.perPage + 1)
+        .on('child_added', (snapshot) => {
+          this.getData(snapshot);
+        });
+
+      this.page += 1;
+    },
+  },
+  watch: {
+    // page: function() {
+    //   console.log(this.page);
+    //   if (this.page === 1) return this.init();
+    //   this.getOpenGroupData();
+    // },
   },
 };
 </script>
