654b73ab0545fc8f8b3bbe34fbbd48c6fc75d644
dudcheol <95_pyc@naver.com>
2021-02-17 22:05:44 +0900

    [#S04P13A308-359] feature/final
    Former-commit-id: 485f93add07cd2b9adcd3ce850924ea31428ded1

--- FrontEnd/src/components/common/card/CommentCard.vue
+++ FrontEnd/src/components/common/card/CommentCard.vue
@@ -2,14 +2,30 @@
   <v-container>
     <v-row>
       <v-col class="d-flex align-center">
-        <v-avatar v-if="data.user.profile" circle size="40">
+        <v-avatar
+          v-if="data.user.profile"
+          circle
+          size="40"
+          style="cursor:pointer"
+          @click="goToProfile"
+        >
           <img :src="data.user.profile.filePath" />
         </v-avatar>
-        <v-avatar v-else circle size="40" color="blue-grey" class="font-weight-medium display-2">
+        <v-avatar
+          v-else
+          circle
+          size="40"
+          color="blue-grey"
+          class="font-weight-medium display-2"
+          style="cursor:pointer"
+          @click="goToProfile"
+        >
           <v-icon color="white">mdi-emoticon-happy</v-icon>
         </v-avatar>
         <div class="d-flex">
-          <h3 class="pl-2 pr-1">{{ data.user.nickname }}</h3>
+          <h3 class="pl-2 pr-1" @click="goToProfile">
+            <span style="cursor:pointer">{{ data.user.nickname }}</span>
+          </h3>
           <v-icon v-if="user.email == data.email" color="error" style="cursor:pointer" @click="del"
             >mdi-close-circle</v-icon
           >
@@ -40,6 +56,10 @@ export default {
   methods: {
     del() {
       this.$emit('del');
+    },
+    goToProfile() {
+      this.$store.commit('SELECTED_USER_ID', this.data.user.nickname);
+      this.$router.push({ name: 'MyPage' });
     },
   },
   created() {
