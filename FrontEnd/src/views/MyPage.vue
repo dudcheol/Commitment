cddef1f2ad287e35d9069904615fb869d0d5aeb8
김청화 <asder36@naver.com>
2021-02-18 10:24:29 +0000

    Update MyPage.vue
    Former-commit-id: 977acdcdcf98523051b3d6f08445d2799a4818a9

--- FrontEnd/src/views/MyPage.vue
+++ FrontEnd/src/views/MyPage.vue
@@ -1,330 +1,123 @@
 <template>
-  <v-tooltip bottom color="transparent" transition="slide-y-transition">
-    <template v-slot:activator="{ on, attrs }">
-      <div :class="boardList.length ? 'external' : 'mt-12'">
-        <div
-          v-if="loading"
-          style="height:55vh"
-          class="d-flex justify-center align-center"
-        >
-          <v-progress-circular
-            :size="70"
-            :width="7"
-            color="blue-grey lighten-4"
-            indeterminate
-          ></v-progress-circular>
-        </div>
+  <v-container class="blue-grey lighten-5 pa-0" fluid>
+    <div class="back_button">
+      <v-btn
+        @click="back"
+        color="white"
+        :ripple="false"
+        fab
+        elevation="3"
+        fixed
+      >
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+    </div>
 
-        <div
-          class="horizontal-scroll-wrapper"
-          v-if="boardList.length"
-          v-bind="attrs"
-          v-on="on"
-        >
-          <div v-for="(item, index) in boardList" :key="index">
-            <div v-if="index % 2 == 0">
-              <div class="img-wrapper slower ">
-                <a target="_blank" rel="noopener">
-                  <img
-                    :src="item.image[0].filePath"
-                    alt=""
-                    class="pola"
-                    @click="moveToDetail(item.id)"
-                  />
-                  <p class="dateFontStyle">{{ item.commit.createdAt }}</p>
-                  <v-spacer></v-spacer>
-                  <p class="areaFontStyle">{{ item.commit.address }}</p>
-                </a>
-              </div>
-            </div>
-            <div v-else-if="index % 3 == 0">
-              <div class="img-wrapper faster slower-down">
-                <a target="_blank" rel="noopener">
-                  <img
-                    :src="item.image[0].filePath"
-                    alt=""
-                    class="pola"
-                    @click="moveToDetail(item.id)"
-                  />
-                  <p class="dateFontStyle">{{ item.commit.createdAt }}</p>
-                  <v-spacer></v-spacer>
-                  <p class="areaFontStyle">{{ item.commit.address }}</p>
-                </a>
-              </div>
-            </div>
-            <div v-else-if="index % 5 == 0">
-              <div class="img-wrapper faster">
-                <a target="_blank" rel="noopener">
-                  <img
-                    :src="item.image[0].filePath"
-                    alt=""
-                    class="pola"
-                    @click="moveToDetail(item.id)"
-                  />
-                  <p class="dateFontStyle">{{ item.commit.createdAt }}</p>
-                  <v-spacer></v-spacer>
-                  <p class="areaFontStyle">{{ item.commit.address }}</p>
-                </a>
-              </div>
-            </div>
-            <div v-else>
-              <div class="img-wrapper slower-down">
-                <a target="_blank" rel="noopener">
-                  <img
-                    :src="item.image[0].filePath"
-                    alt=""
-                    class="pola"
-                    @click="moveToDetail(item.id)"
-                  />
-                  <p class="dateFontStyle">{{ item.commit.createdAt }}</p>
-                  <v-spacer></v-spacer>
-                  <p class="areaFontStyle">{{ item.commit.address }}</p>
-                </a>
-              </div>
-            </div>
-          </div>
+    <v-tooltip bottom color="transparent" transition="slide-y-transition">
+      <template v-slot:activator="{ on, attrs }">
+        <div v-bind="attrs" v-on="on" class="bottom2" id="mobileHidden">
+          <Timeline />
         </div>
+      </template>
+      <span class="blue-grey--text"
+        >오른쪽으로 스크롤하면 더 많은 사진을 볼 수 있어요</span
+      >
+    </v-tooltip>
+    <v-container class="py-8 px-0 white rounded-t-xl mt-12 mt-md-0">
+      <v-row no-gutters>
+        <v-col>
+          <ProfileInfo />
+        </v-col>
+      </v-row>
+
+      <v-row no-gutters class="py-4 pr-md-4">
+        <map-list></map-list>
+      </v-row>
+
+      <div id="pcHidden" class="pt-4">
+        <MyImagesMobile />
       </div>
-    </template>
-    <span class="blue-grey--text"
-      >오른쪽으로 스크롤하면 더 많은 사진을 볼 수 있어요</span
-    >
-  </v-tooltip>
+    </v-container>
+  </v-container>
 </template>
 
 <script>
-import { getUserInfoByNickname } from '../../../api/account';
-import { timelineInfo } from '../../../api/timeline';
+import ProfileInfo from '../components/index/mypage/ProfileInfo';
+import Timeline from '../components/index/mypage/Timeline';
+import MyImagesMobile from '../components/index/mypage/MyImagesMobile';
+import MapList from '../components/index/mypage/MapList.vue';
 import { mapGetters } from 'vuex';
-// import NoDataCard from '../../common/card/NoDataCard.vue';
+
 export default {
-  // components: { NoDataCard },
-  props: ['nickname', 'intro', 'img'],
-  data() {
-    return {
-      boardList: [],
-      //이 아래로는 id를 가지고 searchUserByNickname해서 가져온것
-      email: '',
-      loading: false,
-    };
+  name: 'MyPage',
+  components: {
+    ProfileInfo,
+    Timeline,
+    MyImagesMobile,
+    MapList,
   },
-  activated() {
-    this.boardList = [];
-    this.loading = true;
-    getUserInfoByNickname(
-      this.userId,
-      (response) => {
-        const content = response.data;
-        this.email = content.user.email;
-        timelineInfo(
-          this.email,
-          (response) => {
-            this.loading = false;
-            const res = response.data;
-            for (let i = 0; i < res.length; i++) {
-              const item = res[i];
-              if (item.image[0] == null) {
-                continue;
-              }
-              this.boardList.push(item);
-            }
-          },
-          (error) => {
-            this.loading = false;
-            console.log('timeline에러' + error);
-          }
-        );
-      },
-      (error) => {
-        this.loading = false;
-        console.log('ct에러' + error);
+  data: () => ({
+    benched: 100,
+    mapList: ['전국', '서울', '부산', '경기', '강원', '울산', '광주'],
+    items: ['mdi-home', 'mdi-web', 'mdi-apple-keyboard-control', 'mdi-account'],
+    colors: [
+      'indigo',
+      'warning',
+      'pink darken-2',
+      'red lighten-1',
+      'deep-purple accent-4',
+    ],
+    value1: 1,
+    picked: 0,
+  }),
+  computed: {
+    ...mapGetters({ userId: ['getSelectedUserId'] }),
+    length() {
+      return 7000;
+    },
+    width() {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs':
+          return '500px';
+        case 'sm':
+          return '500px';
+        case 'md':
+          return '500px';
+        case 'lg':
+          return '700px';
+        case 'xl':
+          return '800px';
       }
-    );
-  },
-  methods: {
-    moveToDetail(data) {
-      // this.$router.push({ name: 'Detail', params: { id: data } });
-      this.$store.commit('SELECTED_BOARD_ID', data);
-      this.$router.push({ name: 'Detail' });
+      return 700;
     },
   },
-  computed: {
-    ...mapGetters({
-      user: ['getUserInfo'],
-      userId: ['getSelectedUserId'],
-    }),
+  methods: {
+    back() {
+      this.$router.go(-1);
+    },
+  },
+  activated() {
+    window.scrollTo(0, 0);
   },
 };
 </script>
 
 <style scoped>
-* {
-  box-sizing: border-box;
+@media screen and (max-width: 1000px) {
+  #mobileHidden {
+    display: none;
+  }
 }
-.pola {
-  margin-bottom: 7%;
+@media screen and (min-width: 1000px) {
+  #pcHidden {
+    display: none;
+  }
 }
-/* hide scrollbar */
-::-webkit-scrollbar {
-  width: 1px;
-  height: 1px;
-}
-
-::-webkit-scrollbar-button {
-  width: 1px;
-  height: 1px;
-}
-.external {
-  overflow: hidden;
-  height: 60vh;
-}
-
-.horizontal-scroll-wrapper {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100vh;
-  transform: rotate(-90deg) translate3d(0, -100vh, 0);
-  transform-origin: right top;
-  overflow-y: auto;
-  overflow-x: hidden;
-  padding: 0;
-  height: 100vw;
-  perspective: 1px;
-  transform-style: preserve-3d;
-  padding-bottom: 10rem;
-}
-.img-wrapper {
-  transform: rotate(90deg);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 40vh;
-  transform-origin: 100% 50%;
-  transform: rotate(90deg) translateZ(0.1px) scale(0.9) translateX(0px)
-    translateY(-3vh);
-  transition: 1s;
-}
-
-.img-wrapper:hover {
-  min-height: 65vh;
-}
-
-.slower {
-  transform: rotate(90deg) translateZ(-0.2px) scale(1.1) translateX(50%)
-    translateY(-15vh);
-}
-.slower1 {
-  transform: rotate(90deg) translateZ(-0.25px) scale(1.05) translateX(0%)
-    translateY(3vh);
-}
-.slower2 {
-  transform: rotate(90deg) translateZ(-0.3px) scale(1.3) translateX(0%)
-    translateY(-3vh);
-}
-.slower-down {
-  transform: rotate(90deg) translateZ(-0.2px) scale(1.1) translateX(0%)
-    translateY(0vh);
-}
-.faster {
-  transform: rotate(90deg) translateZ(0.15px) scale(0.8) translateX(0%)
-    translateY(9vh);
-}
-.faster1 {
-  transform: rotate(90deg) translateZ(0.05px) scale(0.8) translateX(0%)
-    translateY(5vh);
-}
-.fastest {
-  transform: rotate(90deg) translateZ(0.22px) scale(0.7) translateX(-10vh)
-    translateY(-20vh);
-}
-.vertical {
-  transform: rotate(90deg) translateZ(-0.15px) scale(1.15) translateX(0%)
-    translateY(0%);
-}
-.last {
-  transform: rotate(90deg) translateZ(-0.2px) scale(1.1) translateX(25vh)
-    translateY(-13vh);
-}
-.scroll-info,
-header {
-  position: absolute;
-  left: 1rem;
-}
-
-header {
-  bottom: 1rem;
-}
-.scroll-info {
-  top: 1rem;
-}
-a {
-  color: inherit;
-  font-weight: 500;
-}
-h1 {
-  font-weight: 300;
-  font-size: 1rem;
-}
-
-.img-wrapper a {
-  overflow: hidden;
+.back_button {
   display: block;
-  padding: 1vh;
-  background: #ffffff;
-  box-shadow: 0 10px 50px #53515082;
-  border-radius: 10px;
-}
-img {
-  max-width: 35vh;
-  max-height: 35vh;
-  transition: 0.5s;
-  vertical-align: top;
-  /* filter: saturate(40%) sepia(30%) hue-rotate(5deg); */
-}
-a:hover img {
-  filter: none;
-}
-p {
-  margin: 0;
-}
-.scroll-info {
-  display: flex;
-  align-items: center;
-}
-.icon svg {
-  width: 50px;
-  fill: currentcolor;
-}
-
-.dateFontStyle {
-  font-family: 'LAB디지털';
-  font-size: 20px;
-}
-.areaFontStyle {
-  font-family: 'BBTreeCL';
-  font-size: 30px;
-  color: gray;
-}
-@font-face {
-  font-family: 'LAB디지털';
-  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/LAB디지털.woff')
-    format('woff');
-  font-weight: normal;
-  font-style: normal;
-}
-@font-face {
-  font-family: 'Cafe24Shiningstar';
-  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.1/Cafe24Shiningstar.woff')
-    format('woff');
-  font-weight: normal;
-  font-style: normal;
-}
-@font-face {
-  font-family: 'BBTreeCL';
-  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_nine_@1.1/BBTreeCL.woff')
-    format('woff');
-  font-weight: normal;
-  font-style: normal;
+  position: absolute;
+  left: 25px;
+  top: 20px;
+  z-index: 1;
 }
 </style>
